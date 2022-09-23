0cfa3ab6f79e9c7e50c824649ae73219ce13fce0
44hukum <ghukumgh@gmail.com>
2022-09-23 12:56:29 +0545

    Initial game development

---
+++ Assets/Scripts/Following_path/E_Distance.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E_Distance : MonoBehaviour
+{
+    public GameObject obj1,obj2;
+    float distance;
+    float obj1_x, obj1_y, obj1_z, obj2_x, obj2_y, obj2_z;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float cost = Vector3.Distance(obj1.transform.position, obj2.transform.position);
+        Debug.Log(cost);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
---
+++ Assets/Scripts/Player controls/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed=10.0f;
+    private float turnspeed = 10.0f;
+    private float horizontainput;
+    private float verticalinput;
+    private int weight = 0;
+    private Rigidbody rb;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    void FixedUpdate() {
+        horizontainput = Input.GetAxis("Horizontal");
+        verticalinput = Input.GetAxis("Vertical");
+
+        //hello world how are you
+        transform.Translate(Vector3.forward * Time.deltaTime * speed* verticalinput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnspeed * horizontainput);
+
+        if (weight == 0) {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+             
+       
+    }
+    private void OnTriggerEnter(UnityEngine.Collider other)
+    {
+        //collectables for agents
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            Debug.Log("Pickedup the resources");
+            transform.Rotate(0, 180, 0);
+
+        }
+        if (other.gameObject.CompareTag("Start"))
+        {
+            Debug.Log("Missed it");
+            transform.Translate(0, 0, 0);
+            transform.Rotate(0, 180, 0);
+            weight = 1;
+
+        }
+
+
+    }
+
+    //counter for methods to remember the source
+
+}
